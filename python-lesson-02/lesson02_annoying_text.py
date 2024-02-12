0724483a5aea211a6b0d51b0d9444bf9d5ee0078
DrKez <kerryn.mctaggart@delwp.vic.gov.au>
2024-02-12 13:17:02 +1100

    assignment 2 completed

---
+++ python-lesson-02/lesson02_annoying_text.py
@@ -0,0 +1,16 @@
+#Practice
+#original_text = "Hello. I have a 10 week old son. He cries "
+#original_text_upper = original_text.upper()
+#shouting_text = "Waaa! "
+#annoying_text = shouting_text * 10
+#print(original_text_upper)
+#original_text_lower = original_text.lower()
+#print(original_text_lower)
+
+#Lesson 02 assignment
+original_text = "Waaa! "
+shouting_text = original_text.upper()
+annoying_text = shouting_text * 10
+print(f"My annoying text is {annoying_text}")
+#print(len(f"My annoying text is {annoying_text}!"))
+print(f"It has {len(f"My annoying text is {annoying_text}!")} characters.")
