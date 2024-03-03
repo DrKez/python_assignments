a073de3a093569db039a3179bd8ab1169cfdcd74
DrKez <kerryn.mctaggart@delwp.vic.gov.au>
2024-03-03 23:04:26 +1100

    Lesson 5_Basic palindrome checker

---
+++ python-lesson-05/lesson05.py
@@ -0,0 +1,11 @@
+#Lesson 5 Palindrome checker
+words = input("Hi there! A palindrome is a word, phrase, or sequence that reads the same backwards as forwards.\n  Please enter a word, phrase or sequence to check if it's a palindrome: ")
+letters = "".join(c for c in words if c.isalpha())
+letters_lower = letters.lower()
+letters_list = list(letters_lower)
+letters_list.reverse()
+string_from_list = "".join(letters_list)
+if letters_lower == string_from_list:
+  print(f"Yes, \"{words}\" is a palindrome.")
+else:
+  print(f"No, \"{words}\" is not a palindrome.")
