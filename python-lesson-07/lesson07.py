9cba6f29db1eceeaed23f70b3a4235a6bc896a7f
DrKez <kerryn.mctaggart@delwp.vic.gov.au>
2024-03-25 13:38:31 +1100

    Lesson7 assignment

--- python-lesson-07/lesson07.py
+++ python-lesson-07/lesson07.py
@@ -3,6 +3,6 @@
 import vowel_counting
 name =  input("\033[1;35m"+"Hello! To find out how many vowels are in your name, please enter your full name: "+"\033[0m")  
 print (f"My full name is {name}.")
-vowel_counting.characters(name)
 print(f"I have a total of {len([char for char in name if char.isalpha()])} letters in my name.")
+vowel_counting.characters(name)
 vowel_counting.each_vowel(name)
